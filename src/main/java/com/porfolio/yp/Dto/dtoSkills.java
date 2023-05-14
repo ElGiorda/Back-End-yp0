6937a4ce048fe2e5dd93c0e185ef5cc96b41dacb
ElGiorda <frangior99@gmail.com>
2023-05-14 13:30:21 -0300

    primer commit

---
+++ src/main/java/com/porfolio/yp/Dto/dtoSkills.java
@@ -0,0 +1,39 @@
+package com.porfolio.yp.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class dtoSkills {
+    
+    @NotBlank
+    private String nombre;
+    @NotBlank
+    private int porcentaje;
+
+    public dtoSkills(String nombre, int porcentaje) {
+        this.nombre = nombre;
+        this.porcentaje = porcentaje;
+    }
+
+    public dtoSkills() {
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(int porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+    
+    
+}
+
---
+++ src/main/java/com/porfolio/yp/Security/Enums/RolNombre.java
@@ -0,0 +1,6 @@
+package com.porfolio.yp.Security.Enums;
+
+
+public enum RolNombre {
+    ROL_ADMIN, ROL_USER
+}
