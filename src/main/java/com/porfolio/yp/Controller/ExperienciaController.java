743a433250798f432427a13f1455e90c8e7f0a16
ElGiorda <frangior99@gmail.com>
2023-05-14 15:20:37 -0300

    arreglo2

--- src/main/java/com/porfolio/yp/Controller/ExperienciaController.java
+++ src/main/java/com/porfolio/yp/Controller/ExperienciaController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://hosting-portfolio-3b1b8.web.app","http://localhost:4200"})
 public class ExperienciaController {
     @Autowired
     ExperienciaService ExperienciaServ;
--- src/main/java/com/porfolio/yp/Controller/ProyectosController.java
+++ src/main/java/com/porfolio/yp/Controller/ProyectosController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200","https://hosting-portfolio-3b1b8.web.app"})
 @RequestMapping("/proyectos")
 public class ProyectosController {
     
